1c9f99132e0c245669242042fbec8757bd67c7e9
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-05 11:58:15 -0500

    done

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,31 @@
 class BuildingsController < ApplicationController
+  def show
+    @building = Building.find(params[:id])
+  end
+
+  def index
+    @buildings = Building.all
+  end
+
+  def edit
+    @building = Building.find(params[:id])
+  end
+
+  def update
+    @building = Building.find(params[:id])
+    @building.assign_attributes(building_params)
+    if @building.valid?
+      @building.save
+      redirect_to @building
+    else
+      flash[:errors] = @building.errors.full_messages
+      redirect_to edit_building_path(@building)
+    end
+  end
+
+  private
+
+  def building_params
+    params.require(:building).permit(:name,:country,:address,:rent_per_floor,:number_of_floors)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :buildings, only: [:show,:index,:edit,:update]
+  resources :companies, only: [:show,:create,:new,:index,:show]
+  resources :employees, only: [:create,:destroy]
+
 end
